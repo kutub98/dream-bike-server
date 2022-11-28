81112d50e77b6a97f72934fc0e65246e2c34c5ea
kutub98 <kutubuddin98710@gmail.com>
2022-11-28 20:46:17 +0600

    implementedUserDeleted

--- index.js
+++ index.js
@@ -2,65 +2,76 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const jwt = require("jsonwebtoken");
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { query } = require("express");
 const uri = `mongodb+srv://${process.env.YDBIKE_NAME}:${process.env.YDBIKE_KEY}@cluster0.mlxcjcs.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // allTypesCollections
-const allCategories = client.db("YDBIKE").collection('BikeCategories')
-const allUsers = client.db("YDBIKE").collection('AllUsers')
-const bikeCollection = client.db("YDBIKE").collection('AllBikes')
-const bookedCollection = client.db("YDBIKE").collection('orderedByBooking')
+const allCategories = client.db("YDBIKE").collection("BikeCategories");
+const allUsers = client.db("YDBIKE").collection("AllUsers");
+const bikeCollection = client.db("YDBIKE").collection("AllBikes");
+const bookedCollection = client.db("YDBIKE").collection("orderedByBooking");
 
-
-
-async function  run(){
-    try{
-        await client.connect()
-        console.log('Dreambike server is running')
-    }
-    catch(error){
-        console.error(error)
-    }
+async function run() {
+  try {
+    await client.connect();
+    console.log("Dreambike server is running");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+// allUser details
+app.post("/allUser", async (req, res) => {
+  const user = req.body;
+  const saveUser = await allUsers.insertOne(user);
+  // console.log(saveUser)
+  res.send(saveUser);
+});
 
-// allUser details 
-app.post('/allUser', async(req, res)=>{
-    const user = req.body;
-    const saveUser = await allUsers.insertOne(user)
-    // console.log(saveUser)
-    res.send(saveUser)
+app.get("/allUser", async (req, res) => {
+  const email = {};
+  const allUser = await allUsers.find(email).toArray();
+  // console.log(allUser)
+  res.send(allUser);
+});
+
+app.get("/allUser/:userRole", async (req, res) => {
+  const user = req.params.userRole;
+  const query = { userRole: user };
+  const result = await allUsers.find(query).toArray();
+  res.send(result);
+});
+
+app.get("/allUser/:email", async (req, res) => {
+  const user = req.params.email;
+  const query = { email: user };
+  const result = await allUsers.findOne(query).toArray();
+  res.send(result);
+});
+
+app.delete('/allUser/:id', async(req, res)=> {
+     const id = req.params.email;
+     const query = {email : id};
+     const result = await allUsers.deleteOne(query)
+     console.log(result)
+     res.send(result)
+    
 })
-
-
-app.get('/allUser', async(req, res)=>{
-    const email= {}
-    const allUser = await allUsers.find(email).toArray();
-    // console.log(allUser)
-    res.send(allUser)
-})
-
-app.get("/allUsers/:userRole", async (req, res) => {
-    const user = req.params.userRole;
-    const query = { userRole:  user};
-    const result = await allUsers.find(query).toArray();
-    res.send(result);
-  });
-
 
 //   app.put("/allUser/userRole/:id", async (req, res) => {
-   
+
 //     const id = req.params.id;
 //     const filter = { _id: ObjectId(id) };
 //     const option = { upsert: true };
@@ -80,88 +91,73 @@ app.get("/allUsers/:userRole", async (req, res) => {
 //     res.send({ isAdmin: user?.role === "admin" });
 //   });
 
-
-
-
-
-
-
 // getAllCategories
-app.get('/allCategories', async(req, res)=>{
-    const  query ={};
-     const result = await allCategories.find(query).toArray()
-     res.send(result)
- })
+app.get("/allCategories", async (req, res) => {
+  const query = {};
+  const result = await allCategories.find(query).toArray();
+  res.send(result);
+});
 // getAllBikesCollection
- app.get('/allBikes', async(req, res)=>{
-    const query = {}
-    const result = await bikeCollection.find(query).toArray()
-    res.send(result)
-})
+app.get("/allBikes", async (req, res) => {
+  const query = {};
+  const result = await bikeCollection.find(query).toArray();
+  res.send(result);
+});
 // posting a product
- app.post('/allBikes', async(req, res)=>{
-    const query = req.body;
-    const result = await bikeCollection.insertOne(query)
-    res.send(result)
-})
+app.post("/allBikes", async (req, res) => {
+  const query = req.body;
+  const result = await bikeCollection.insertOne(query);
+  res.send(result);
+});
 // getAllBikesCollectionServiceByID
 app.get("/allCategories/:CategoryName", async (req, res) => {
-    const id = req.params.CategoryName;
-    const query = {CategoryName : id}
-    const serviceCategory = await  bikeCollection.find(query).toArray()
-    // console.log(serviceCategory)
-    res.send(serviceCategory);
+  const id = req.params.CategoryName;
+  const query = { CategoryName: id };
+  const serviceCategory = await bikeCollection.find(query).toArray();
+  // console.log(serviceCategory)
+  res.send(serviceCategory);
 });
 app.get("/productId/:id", async (req, res) => {
-    const id = req.params.id;
-    const query = {_id: ObjectId(id)}
-    const productId = await  bikeCollection.find(query).toArray()
-    // console.log(productId,"productID")
-    res.send(productId);
+  const id = req.params.id;
+  const query = { _id: ObjectId(id) };
+  const productId = await bikeCollection.find(query).toArray();
+  // console.log(productId,"productID")
+  res.send(productId);
 });
 
 app.get("/myProduct/:email", async (req, res) => {
-    const email = req.params.email;
-    const query = {email: email}
-    const myProduct = await  bikeCollection.find(query).toArray()
-    console.log(myProduct,"productID")
-    res.send(myProduct);
+  const email = req.params.email;
+  const query = { email: email };
+  const myProduct = await bikeCollection.find(query).toArray();
+  console.log(myProduct, "productID");
+  res.send(myProduct);
 });
 
 // gettingOrderByBooking
 
-app.post('/booked', async(req, res)=>{
-    const query = req.body;
-    const receivedOrderByBooking = await bookedCollection.insertOne(query)
-    // console.log(receivedOrderByBooking)
-    res.send(receivedOrderByBooking)
-})
+app.post("/booked", async (req, res) => {
+  const query = req.body;
+  const receivedOrderByBooking = await bookedCollection.insertOne(query);
+  // console.log(receivedOrderByBooking)
+  res.send(receivedOrderByBooking);
+});
 
 // fine total order by per user
 
+app.get("/ordered/:email", async (req, res) => {
+  const id = req.params.email;
+  const query = { customerEmail: id };
+  const receivedAllOrder = await bookedCollection.find(query).toArray();
+  // console.log(receivedAllOrder)
+  res.send(receivedAllOrder);
+});
 
-app.get('/ordered/:email', async(req, res)=>{
-    const id = req.params.email;
-    const query = {customerEmail: id};
-    const receivedAllOrder = await bookedCollection.find(query).toArray()
-    // console.log(receivedAllOrder)
-    res.send(receivedAllOrder)
-})
+run().catch((error) => console.error(error));
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-run().catch(error => console.error(error))
-
-
-
-
-
-
-
-
-app.get('/', (req, res)=>{
-    res.send('Server is running')
-})
-
-app.listen(port, (req, res)=>{
-    console.log(`dream bike is running ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`dream bike is running ${port}`);
+});
